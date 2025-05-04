7c530f911af09f1f0f141daa197a7bf5c140fe0d
sadhin28 <taosifsadhin113@gmail.com>
2025-05-05 02:28:49 +0600

    add about button

--- src/components/Cartscontainer/Cartcontainer.jsx
+++ src/components/Cartscontainer/Cartcontainer.jsx
@@ -1,7 +1,16 @@
+import About from "../About/About";
+import Cart from "../Cart/Cart";
 const Cartcontainer = () => {
+
     return (
         <div className=" text-center w-1/3 border border-amber-400">
             <h1 className="text-2xl font-bold">Cart Container</h1>
+            <div className="flex justify-center gap-5 mt-5 ">
+                <button className="btn">CART</button>
+                <button className="btn">ABOUT</button>
+            </div>
+            <Cart></Cart>
+            <About></About>
         </div>
     );
 };
