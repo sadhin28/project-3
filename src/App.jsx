3ef1cf3af44dcca9f39a57269fbfda0c4759d17b
sadhin28 <taosifsadhin113@gmail.com>
2025-05-05 02:40:46 +0600

    add card status

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Allproducts from './components/Allproducts/Allproducts'
 import Cartcontainer from './components/Cartscontainer/Cartcontainer'
+import { useState } from 'react'
+
 function App() {
+ const [isactive,setisactive]=useState({
+  cart : true,
+  Status:'active'
+ })
+const handleisActive =(Status)=>{
+    if(Status == 'cart'){
+        setisactive({
+           cart : true,
+          Status:'active'
+        })
+    }
+    else{
+      setisactive({
+         cart :false,
+        Status:'active'
+      })
+    }
+}
   return (
     <div>
     <Navbar></Navbar>
