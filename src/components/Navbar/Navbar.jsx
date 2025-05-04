f9fc2ec7be6febcb14cbbe320680a8182549df63
sadhin28 <taosifsadhin113@gmail.com>
2025-05-05 01:58:12 +0600

    add nav bar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+
+        <div className=" px-10 p-8 flex justify-between items-center navbar bg-base-100 shadow-sm">
+
+            <div>
+                <h1 className='font-bold  lg:text-3xl md:text-2xl'>LOGO</h1>
+            </div>
+
+            <div>
+
+                <ul className='font-bold  lg:text-3xl md:text-2xl flex items-center lg:gap-10 md:gap-6 gap-5'>
+                    <li>HOME</li>
+                    <li>PRODUCT</li>
+                    <li>CART</li>
+                    <li>$ 500</li>
+                </ul>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Navbar;
