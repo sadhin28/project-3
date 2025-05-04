f1965be2a86ebb9399ef617165d7449f425c5020
sadhin28 <taosifsadhin113@gmail.com>
2025-05-05 03:32:20 +0600

    add buttne togle

--- src/components/Cartscontainer/Cartcontainer.jsx
+++ src/components/Cartscontainer/Cartcontainer.jsx
@@ -1,17 +1,20 @@
 import About from "../About/About";
 import Cart from "../Cart/Cart";
+import './Cartcontainer.css'
 const Cartcontainer = ({handleisActive,isactive}) => {
-     console.log(isactive)
+    //  console.log(isactive)
 //    console.log(handleisActive)
     return (
         <div className=" text-center w-1/3 border border-amber-400">
             <h1 className="text-2xl font-bold">Cart Container</h1>
             <div className="flex justify-center gap-5 mt-5 ">
-                <button onClick={()=>handleisActive('cart')} className="btn">CART</button>
-                <button onClick={()=>handleisActive('about')} className="btn">ABOUT</button>
+                <button onClick={()=>handleisActive('cart')} className={` ${isactive.cart?'btn active':'btn'}`}>CART</button>
+                <button onClick={()=>handleisActive('about')} className={`${isactive.cart?'btn':'active btn'}`}>ABOUT</button>
             </div>
             <div>
-                    
+                    {
+                        isactive.cart?<Cart></Cart>:<About></About>
+                    }
             </div>
         </div>
     );
